dd51e0bd463bf4cd0c9b6681893d1d08f26a400c
Chandy <ChandlerG@users.noreply.github.com>
2018-07-04 16:30:10 -0700

    Dagwood displayed eyyyy hes so cute

---
+++ macra/src/Dagwood.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Dagwood.css';
+
+class Dagwood extends React.Component{
+    render() {
+        return (<img className="center-fit" src="dagwood.jpg"/>)
+    }
+}
+
+export default Dagwood;
+
--- macra/src/index.js
+++ macra/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Dagwood from './Dagwood';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Dagwood />, document.getElementById('cuteCatDagwood'));
 registerServiceWorker();
