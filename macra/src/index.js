6e5f04f4c155a5d5031a239ee9eba641607102db
Chandy <ChandlerG@users.noreply.github.com>
2018-07-04 16:42:17 -0700

    Refactor so app.js does all the things

--- macra/src/index.js
+++ macra/src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Dagwood from './Dagwood';
-import Sully from './Sully';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Dagwood />, document.getElementById('cuteCatDagwood'));
-ReactDOM.render(<Sully />, document.getElementById('cuteCatSully'));
 registerServiceWorker();
