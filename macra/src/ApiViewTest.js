96ac8a856a30c0d9f515a43a28863c9e22ea1df3
ChandlerG <ChandlerG@users.noreply.github.com>
2018-08-03 16:03:13 -0700

    refactor to use await

--- macra/src/ApiViewTest.js
+++ macra/src/ApiViewTest.js
@@ -17,19 +17,11 @@ export default class ApiViewTest extends React.Component
   componentDidMount = async () => {
     let result = 'hi';
     let imageTag;
-    catCall().then(
-      (res) => {
-        return res.text()
-      }
-    ).then((res) => {
-      result = res;
-      //console.log(result);
-      imageTag = extractImg(result);
-      console.log(imageTag[1]);
-      imageTag = imageTag[1];
-      console.log(imageTag);
-       this.setState({imgUrl: imageTag});
-    });
+    const res = await catCall();
+    const resText = await res.text()
+    const extractedImageArray = extractImg(resText);
+    const extractedImage = extractedImageArray[1];
+    this.setState({imgUrl: extractedImage});
   }
 }
 
